dcd51f44b0eec3ee11baa42499b7ee5f883b36a4
Christopher Viel <viel.christopher@gmail.com>
2018-07-06 18:27:46 -0400

    refactor(platforms): regroup with core and move to packages/platforms/

--- update_hass.sh
+++ update_hass.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-source /var/lib/homeassistant/bin/activate.fish
+source /var/lib/homeassistant/bin/activate
 pip3 install --upgrade homeassistant
 deactivate
 /var/lib/homeassistant/bin/hass --script check_config
